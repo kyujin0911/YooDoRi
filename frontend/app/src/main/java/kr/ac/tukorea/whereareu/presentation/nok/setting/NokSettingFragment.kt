25e25cc7a007bb57bf95a4e9ec7af257caae651d
DoDi <4717jun57@naver.com>
2024-04-03 18:53:04 +0900

    fix: 설정화면 수정

--- frontend/app/src/main/java/kr/ac/tukorea/whereareu/presentation/nok/setting/NokSettingFragment.kt
+++ frontend/app/src/main/java/kr/ac/tukorea/whereareu/presentation/nok/setting/NokSettingFragment.kt
@@ -12,12 +12,13 @@ import kr.ac.tukorea.whereareu.presentation.nok.home.NokHomeViewModel
 import kr.ac.tukorea.whereareu.util.extension.repeatOnStarted
 
 @AndroidEntryPoint
-class NokSettingFragment: BaseFragment<FragmentNokSettingBinding>(R.layout.fragment_nok_setting) {
+class NokSettingFragment : BaseFragment<FragmentNokSettingBinding>(R.layout.fragment_nok_setting) {
     private val viewModel: NokHomeViewModel by activityViewModels()
     private val settingViewModel: SettingViewModel by activityViewModels()
 
     override fun initObserver() {
     }
+
     override fun initView() {
         val spf = requireActivity().getSharedPreferences("User", MODE_PRIVATE)
         val otherSpf = requireActivity().getSharedPreferences("OtherUser", MODE_PRIVATE)
@@ -35,16 +36,16 @@ class NokSettingFragment: BaseFragment<FragmentNokSettingBinding>(R.layout.fragm
         }*/
 
         binding.updateTimeBtn.setOnClickListener {
-            val dialog = SetUpdateTimeDialogFragment{time ->
+            val dialog = SetUpdateTimeDialogFragment { time ->
                 viewModel.setUpdateDuration(time.toLong())
                 binding.updateTimeTv.text = time
             }
             dialog.show(childFragmentManager, dialog.tag)
         }
 
-        binding.updateUserInfoBtn.setOnClickListener{
+        binding.updateUserInfoBtn.setOnClickListener {
             onClickUpdateUserInfo()
-            Log.d("UpdateUserInfoBtn","Clicked")
+            Log.d("UpdateUserInfoBtn", "Clicked")
         }
     }
 
@@ -58,14 +59,18 @@ class NokSettingFragment: BaseFragment<FragmentNokSettingBinding>(R.layout.fragm
 
         repeatOnStarted {
             Log.d("Nok Setting Fragment", "repeatOnStarted")
-            settingViewModel.userInfo.collect{
+            settingViewModel.userInfo.collect {
                 Log.d("Nok_Setting_Fragment", "get User Info API")
 
                 val nokName = it.nokInfoRecord.nokName
                 val nokPhone = it.nokInfoRecord.nokPhoneNumber
                 val dementiaName = it.dementiaInfoRecord.dementiaName
                 val dementiaPhone = it.dementiaInfoRecord.dementiaPhoneNumber
-                Log.d("Nok_Setting_Fragment", "$nokName, $nokPhone, $dementiaName,$dementiaPhone")
+
+                binding.userNameTv.text = nokName
+                binding.userPhoneNumberTv.text = nokPhone
+                binding.otherNameTv.text = dementiaName
+                binding.otherPhoneTv.text = dementiaPhone
             }
         }
 
@@ -74,11 +79,8 @@ class NokSettingFragment: BaseFragment<FragmentNokSettingBinding>(R.layout.fragm
         binding.otherNameTv.text = otherSpf.getString("name", "")
         binding.otherPhoneTv.text = otherSpf.getString("phone", "")
     }
-    fun onClickUpdateUserInfo(){
+
+    fun onClickUpdateUserInfo() {
         findNavController().navigate(R.id.action_nokSettingFragment_to_modifyUserInfoFragment2)
-    }
-
-    fun getUserInfo(){
-
     }
 }
