fab26ed51478e7bb44d0f87010d56f594cb8b190
DoDi <4717jun57@naver.com>
2024-04-06 18:11:20 +0900

    fix: 위치 정보 업데이트 시간 설정 화면 수정
    NumberPicker가 아닌 리스트 형식으로 수정

--- frontend/app/src/main/java/kr/ac/tukorea/whereareu/presentation/nok/setting/updateTime/SettingUpdateTimeFragment.kt
+++ frontend/app/src/main/java/kr/ac/tukorea/whereareu/presentation/nok/setting/updateTime/SettingUpdateTimeFragment.kt
@@ -1,4 +1,4 @@
-package kr.ac.tukorea.whereareu.presentation.nok.setting
+package kr.ac.tukorea.whereareu.presentation.nok.setting.updateTime
 
 import androidx.navigation.fragment.findNavController
 import kr.ac.tukorea.whereareu.R
@@ -12,13 +12,8 @@ class SettingUpdateTimeFragment(val setUpdateTime: (String) -> Unit): BaseDialog
     }
 
     override fun initView() {
-//        val nokUpdatePhone = requireActivity().getSharedPreferences("UpdateTime", Context.MODE_PRIVATE)
         with(binding) {
-            numberPicker.minValue = 0
-            numberPicker.maxValue = 8
-
-            numberPicker.displayedValues =
-                arrayOf("1", "3", "5", "10", "15", "20", "30", "45", "60")
+            
         }
 
 
