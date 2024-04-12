3154caa71c771109bec9c2e65f9ebaf7dcd96e0d
박승호 <joinus0409@naver.com>
2024-04-12 22:03:58 +0900

    feat: Request body 예시 추가, Error Response 형식 추가

--- backend/fastapi/app/bodymodel.py
+++ backend/fastapi/app/bodymodel.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional, Dict, Optional, Union, List
+from fastapi import HTTPException
 
 # Define request and response models
 class CommonResponse(BaseModel):
@@ -25,9 +26,9 @@ class nokResult(BaseModel):
     nokKey: str
 
 class ReceiveNokInfoRequest(BaseModel):
-    keyFromDementia : int
-    nokName : str
-    nokPhoneNumber : str
+    keyFromDementia : int = Field(examples=["123456"])
+    nokName : str = Field(examples=["홍길동"])
+    nokPhoneNumber : str = Field(examples=["010-1234-5678"])
 
 class ReceiveNokInfoResponse(BaseModel):
     status: str
@@ -39,8 +40,8 @@ class dementiaResult(BaseModel):
     dementiaKey: str
 
 class ReceiveDementiaInfoRequest(BaseModel):
-    name : str
-    phoneNumber : str
+    name : str = Field(examples=["성춘향"])
+    phoneNumber : str = Field(examples=["010-1234-5678"])
 
 class ReceiveDementiaInfoResponse(BaseModel):
     status: str
@@ -52,7 +53,7 @@ class connectionResult(BaseModel):
     nokInfoRecord: nokInfoRecord
 
 class ConnectionRequest(BaseModel):
-    dementiaKey : int
+    dementiaKey : int = Field(examples=["123456"])
 
 class ConnectionResponse(BaseModel):
     status: str
@@ -60,25 +61,25 @@ class ConnectionResponse(BaseModel):
     result: connectionResult
 
 class loginRequest(BaseModel):
-    key : int
-    isDementia : int
+    key : int = Field(examples=["123456"])
+    isDementia : int = Field(examples=["1"])
 
 class ReceiveLocationRequest(BaseModel):
-    dementiaKey : int
-    date : str
-    time : str
-    latitude : float
-    longitude : float
-    bearing : float
-    accelerationSensor : List[float] # list of 3 floats
-    gyroSensor : List[float] #list of 3 floats
-    directionSensor : List[float] #list of 3 floats
-    lightSensor : List[float]
-    battery : int
-    isInternetOn : bool
-    isGpsOn : bool
-    isRingstoneOn : int
-    currentSpeed : float
+    dementiaKey : int = Field(examples=["123456"])
+    date : str = Field(examples=["2024-03-19"])
+    time : str = Field(examples=["12:00:00"])
+    latitude : float = Field(examples=["37.123456"])
+    longitude : float = Field(examples=["127.123456"])
+    bearing : float = Field(examples=["0.0"])
+    accelerationSensor : List[List[float]] = Field(..., examples=[[-1.84068, 6.68136, 6.0359]])
+    gyroSensor : List[float] = Field(..., examples=[[-1.84068, 6.68136, 6.0359]])
+    directionSensor : List[float] = Field(..., examples=[[-1.84068, 6.68136, 6.0359]])
+    lightSensor : List[float] = Field(examples=[500.0])
+    battery : int = Field(examples=["100"])
+    isInternetOn : bool = Field(examples=["true"])
+    isGpsOn : bool = Field(examples=["true"])
+    isRingstoneOn : int = Field(examples=["1"])
+    currentSpeed : float = Field(examples=["0.0"])
 
 class LastLoc(BaseModel):
     latitude : float
@@ -97,21 +98,19 @@ class GetLocationResponse(BaseModel):
     message: str
     result: LastLoc
 
-
-
 class ModifyUserInfoRequest(BaseModel):
-    key : int
-    isDementia : int
-    name : str
-    phoneNumber : str
+    key : int = Field(examples=["123456"])
+    isDementia : int = Field(examples=["1"])
+    name : str = Field(examples=["김이름"])
+    phoneNumber : str = Field(examples=["010-1234-5678"])
 
 class ModifyUserUpdateRateRequest(BaseModel):
-    key : int
-    isDementia : int
-    updateRate : int
+    key : int = Field(examples=["123456"])
+    isDementia : int = Field(examples=["1"])
+    updateRate : int = Field(examples=["15"])
 
 class AverageWalkingSpeedRequest(BaseModel):
-    dementiaKey : int
+    dementiaKey : int = Field(examples=["123456"])
 
 class AverageAndLastLoc(BaseModel):
     averageSpeed : float
@@ -155,3 +154,11 @@ class LocHistoryResponse(BaseModel):
     status: str
     message: str
     result : LocHisRecord
+
+class CustomHTTPException(HTTPException):
+    def __init__(self, status_code: int, detail: str):
+        super().__init__(status_code=status_code, detail={"status": "error", "message": detail})
+
+class ErrorResponse(BaseModel):
+    status: str
+    message: str
