6dc26ce2d66031965538be3364e45820dda258b2
Dodi029 <4717jun57@naver.com>
2024-04-18 14:36:39 +0900

    fix : 보호대상자 설정 화면 오류 수정
    연결 안되던 문제 해결

--- frontend/app/src/main/java/kr/ac/tukorea/whereareu/data/model/setting/ModifyUserInfoRequest.kt
+++ frontend/app/src/main/java/kr/ac/tukorea/whereareu/data/model/setting/ModifyUserInfoRequest.kt
@@ -1,8 +1,8 @@
 package kr.ac.tukorea.whereareu.data.model.setting
 
 data class ModifyUserInfoRequest(
-    val isDementia : Int,
     val key : String,
+    val isDementia : Int,
     val name : String,
     val phoneNumber : String,
 )
