fd14b6f301710d1179d9c095a408c06628677ea3
KKJ <kyujin0911@tukorea.ac.kr>
2024-05-08 01:04:54 +0900

    chore: 필요없는 파일 제거 및 패키지 구조화

--- frontend/app/src/main/java/kr/ac/tukorea/whereareu/presentation/nok/setting/adapter/UpdateRateRVA.kt
+++ frontend/app/src/main/java/kr/ac/tukorea/whereareu/presentation/nok/setting/adapter/UpdateRateRVA.kt
@@ -1,4 +1,4 @@
-package kr.ac.tukorea.whereareu.presentation.nok.setting.updateTime
+package kr.ac.tukorea.whereareu.presentation.nok.setting.adapter
 
 import android.view.LayoutInflater
 import android.view.View
