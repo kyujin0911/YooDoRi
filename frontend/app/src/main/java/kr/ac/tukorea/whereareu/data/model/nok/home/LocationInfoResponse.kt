f499dab52b481e2b03f98c78d574192cb8019edf
KKJ <kyujin0911@tukorea.ac.kr>
2024-05-10 17:00:54 +0900

    refactor: api 명세서 변경사항 반영

--- frontend/app/src/main/java/kr/ac/tukorea/whereareu/data/model/nok/home/LocationInfoResponse.kt
+++ frontend/app/src/main/java/kr/ac/tukorea/whereareu/data/model/nok/home/LocationInfoResponse.kt
@@ -7,9 +7,9 @@ data class LocationInfoResponse(
     val isRingstoneOn: Int,
     val latitude: Double,
     val longitude: Double,
-    val userStatus: Int,
+    val userStatus: String,
     val bearing: Float,
     val currentSpeed: Float
 ){
-    constructor(): this(100, true, true, 0, 37.5666103, 126.9783882, 1, 0f, 0f)
+    constructor(): this(100, true, true, 0, 37.5666103, 126.9783882, "", 0f, 0f)
 }
--- frontend/app/src/main/java/kr/ac/tukorea/whereareu/domain/home/DementiaStatusInfo.kt
+++ frontend/app/src/main/java/kr/ac/tukorea/whereareu/domain/home/DementiaStatusInfo.kt
@@ -1,11 +1,11 @@
 package kr.ac.tukorea.whereareu.domain.home
 
 data class DementiaStatusInfo(
-    val userStatus: Int,
+    val userStatus: String,
     val battery: Int,
     val isGpsOn: Boolean,
     val isInternetOn: Boolean,
     val isRingstoneOn: Int,
 ){
-    constructor(): this(0,100,false,false,0)
+    constructor(): this("",100,false,false,0)
 }
