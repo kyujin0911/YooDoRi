dca2d5545d0d9346a05fcf72c92374a745b0a43b
박승호 <joinus0409@naver.com>
2024-05-18 20:02:48 +0900

    fix: fcm test code 수정

--- backend/fastapi/app/fcm_notification.py
+++ backend/fastapi/app/fcm_notification.py
@@ -5,9 +5,18 @@ push_service = FCMNotification(Config.fcm_server_key)
 
 
 
-def send_push_notification(token, body, title):
+def send_push_notification(token, body, title, data):
     
-    result = push_service.notify_single_device(registration_id=token, message_title=title, message_body=body)
+    #result = push_service.notify_single_device(registration_id=token, message_title=title, message_body=body)
+
+    result = push_service.notify_single_device(
+        registration_id=token,
+        message_title=title,
+        message_body=body,
+        data_message={
+            "이도영": data
+        }
+    )
 
     return result
 
