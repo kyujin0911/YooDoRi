353f83cde1bd5ce4a1cc852ddc037ffe63f0bede
박승호 <joinus0409@naver.com>
2024-05-24 14:54:19 +0900

    fix: fcm 알림 푸시 함수 비동기 처리

--- backend/fastapi/app/validater.py
+++ backend/fastapi/app/validater.py
@@ -7,7 +7,7 @@ class validateInSafeArea:
     def __init__(self):
         pass
     
-    async def isinsafearea(self, current_loc, safe_area_list):
+    def isinsafearea(self, current_loc, safe_area_list):
         distance_list = []
         for safe_area in safe_area_list:
             safe_area_location = (safe_area.latitude, safe_area.longitude)
@@ -24,7 +24,7 @@ class validateInSafeArea:
         else:
             return nearest_safe_area, True
     
-    async def pushNotification(self, latest_location, before_location, safeArea):
+    async def pushNotification(self, fcm_token, latest_location, before_location, safeArea):
         if latest_location.isInSafeArea == 0 and before_location.isInSafeArea == 1:
             data = {
                 "safeAreaName" : safeArea.area_name,
@@ -36,6 +36,6 @@ class validateInSafeArea:
                 "safeAreaName" : safeArea.area_name,
                 "time" : latest_location.time
             }
-            send_push_notification(Config.temp_fcm_token, "어디U", "안심 구역 진입", data)
+            send_push_notification(fcm_token, "어디U", "안심 구역 진입", data)
     
 
